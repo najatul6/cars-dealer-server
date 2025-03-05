8103cc931f467be19944bcea118189c14b34743e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-05 21:40:55 +0600

    add: implement endpoint to retrieve all users with admin verification

--- index.js
+++ index.js
@@ -84,6 +84,11 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/createUser", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
